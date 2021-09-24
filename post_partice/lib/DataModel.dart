ca53fad751ed2d244baedcd37fb87bcaf7088ea4
ramimhossain12 <ramimhossain6242@gmail.com>
2021-09-25 01:39:43 +0600

    Post Request

---
+++ post_partice/lib/DataModel.dart
@@ -0,0 +1,34 @@
+
+import 'dart:convert';
+
+DataModel dataModelFromJson(String str) => DataModel.fromJson(json.decode(str));
+
+String dataModelToJson(DataModel data) => json.encode(data.toJson());
+
+class DataModel {
+  DataModel({
+    required this.name,
+    required this.job,
+    required this.id,
+    required this.createdAt,
+  });
+
+  String name;
+  String job;
+  String id;
+  DateTime createdAt;
+
+  factory DataModel.fromJson(Map<String, dynamic> json) => DataModel(
+    name: json["name"],
+    job: json["job"],
+    id: json["id"],
+    createdAt: DateTime.parse(json["createdAt"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "job": job,
+    "id": id,
+    "createdAt": createdAt.toIso8601String(),
+  };
+}
